4e607669e83a8691f34d5a4464990e1319928ad1
LucasJS331 <69942512+LucasJS331@users.noreply.github.com>
2021-07-18 15:25:01 -0300

    mudança de arquitetura

--- db/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('games_api', 'root', '123456', {
-    host: 'localhost',
-    dialect: 'mysql',
-
-})
-
-module.exports = connection;
